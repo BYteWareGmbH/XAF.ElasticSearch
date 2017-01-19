05b59827a4e83c10b2ab929c3d82b16adf3bdd06
V.Negi <V.Negi@VNEGI-PC>
2017-01-19 10:02:12 +0100

    Added Elastic Search Index Classes

---
+++ BYteWare.XAF.ElasticSearch/BusinessObjects/ElasticSearchIndex.cs
@@ -0,0 +1,64 @@
+﻿namespace BYteWare.XAF.ElasticSearch.BusinessObjects
+{
+    using System;
+    using DevExpress.Persistent.Validation;
+    using DevExpress.Xpo;
+    using DevExpress.Xpo.Metadata;
+
+    public class ElasticSearchIndex : XPObject, IElasticSearchIndex
+    {
+        public ElasticSearchIndex() : base()
+        {
+            // This constructor is used when an object is loaded from a persistent storage.
+            // Do not place any code here.
+        }
+        private bool _Active;
+        private string _Name;
+
+        /// <summary>
+        /// Ist der Index aktiv 
+        /// </summary>
+        public bool Active
+        {
+            get
+            {
+                return _Active;
+            }
+            set
+            {
+                SetPropertyValue("Active", ref _Active, value);
+            }
+        }
+
+        /// <summary>
+        /// Name des Index
+        /// </summary>
+        [Size(255)]
+        [RuleRequiredField, RuleUniqueValue]
+        [Indexed(Unique = true)]
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                SetPropertyValue("Name", ref _Name, value);
+            }
+        }
+
+        /// <summary>
+        /// Indexes Refersh after synchronization
+        /// </summary>
+        [Association, DevExpress.Xpo.Aggregated]
+        public XPCollection<ElasticSearchIndexUpdate> ElasticSearchIndexesUpdate
+        {
+            get
+            {
+                return GetCollection<ElasticSearchIndexUpdate>("ElasticSearchIndexesUpdate");
+            }
+        }
+    }
+
+}
---
+++ BYteWare.XAF.ElasticSearch/BusinessObjects/ElasticSearchIndexUpdate.cs
@@ -0,0 +1,69 @@
+﻿namespace BYteWare.XAF.ElasticSearch.BusinessObjects
+{
+    using System;
+    using System.ComponentModel;
+    using DevExpress.Persistent.Validation;
+    using DevExpress.Xpo;
+
+    /// <summary>
+    /// IndexAktualisieren Business Klasse.
+    /// </summary>
+    [DeferredDeletion(false)]
+    [DefaultProperty("Index")]
+    public sealed class ElasticSearchIndexUpdate : XPObject
+    {
+        /// <summary>
+        /// Initialisiert eine neue Instanz der <see cref="ElasticSearchIndexUpdate" /> Klasse.
+        /// </summary>
+        /// <param name="session">XPO session</param>
+        public ElasticSearchIndexUpdate(Session session)
+            : base(session)
+        {
+        }
+
+        /// <summary>
+        /// Initialisiert die Eigenschaften einer neu erzeugten Instanz der <see cref="ElasticSearchIndexUpdate" /> Klasse.
+        /// </summary>
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+        }
+
+        // Fields...
+        private ElasticSearchIndex _Index;
+        private DateTime _datetime;
+
+        /// <summary>
+        /// Zeitpunkt der Synchronisierung
+        /// </summary>
+        public DateTime Datetime
+        {
+            get
+            {
+                return _datetime;
+            }
+            set
+            {
+                SetPropertyValue("Datetime", ref _datetime, value);
+            }
+        }
+
+        /// <summary>
+        /// Index der aktualisiert werden muss
+        /// </summary>
+        [Association]
+        [RuleRequiredField]
+        public ElasticSearchIndex Index
+        {
+            get
+            {
+                return _Index;
+            }
+            set
+            {
+                SetPropertyValue("Index", ref _Index, value);
+            }
+        }
+    }
+
+}
