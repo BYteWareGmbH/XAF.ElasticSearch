da1b48c392c748b23591230647a490a98d2ef591
Markus Dütting <duetting@cosymed.de>
2017-10-09 18:43:09 +0200

    Nuget Versionen erhöhen, Jenkins hat die Verzeichnisse neu erstellt und damit funktioniert das dynamische Pushen der Pakete nicht mehr, da sie nicht mehr existieren.

--- BYteWare.XAF.ElasticSearch/Properties/AssemblyInfo.cs
+++ BYteWare.XAF.ElasticSearch/Properties/AssemblyInfo.cs
@@ -24,4 +24,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("16.24.20")]
-[assembly: AssemblyInformationalVersion("16.24.20.7")]
+[assembly: AssemblyInformationalVersion("16.24.20.8")]
